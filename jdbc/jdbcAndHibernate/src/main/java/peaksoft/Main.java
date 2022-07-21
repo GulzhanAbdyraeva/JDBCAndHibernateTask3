4fe6634921023098ce9aac4f54de8681142390ca
user <imaginejuli@gmail.com>
2022-07-21 18:05:17 +0300

    method added

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,18 @@
 package peaksoft;
 
+import peaksoft.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("Gulzhan", "Abdyraeva", (byte) 20);
+        System.out.println(us.getAllUsers());
+//        us.getAllUsers().forEach(System.out::println);
+        us.cleanUsersTable();
+        us.removeUserById(1);
+        us.dropUsersTable();
+
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
@@ -1,7 +1,10 @@
 package peaksoft.dao;
 
 import peaksoft.model.User;
+import peaksoft.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJdbcImpl implements UserDao {
@@ -9,28 +12,90 @@ public class UserDaoJdbcImpl implements UserDao {
     public UserDaoJdbcImpl() {
 
     }
+    public final Util util = new Util();
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS users(" +
+                "id SERIAL PRIMARY KEY," +
+                "name VARCHAR," +
+                "last_name VARCHAR," +
+                "age INT NOT NULL);";
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+            System.out.println("The table create successful.");
+        } catch (SQLException e) {
+            System.out.println("NOT CREATE!");
+        }
     }
 
     public void dropUsersTable() {
-
+        String dropUsers = "DROP TABLE IF EXISTS users;";
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(dropUsers);
+            System.out.println("The table is dropped!");
+        } catch (SQLException e) {
+            System.out.println("Not drop!");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String save = "INSERT INTO users(name, last_name, age) VALUES(?,?,?)";
+        try (Connection connection = util.getConnection();
+             PreparedStatement statement = connection.prepareStatement(save)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+            System.out.println(name + " " + "added");
+        } catch (SQLException e) {
+            System.out.println("!!!");
+        }
     }
 
-    public void removeUserById(long id) {
 
+    public void removeUserById(long id) {
+        String remove = "DROP TABLE IF EXISTS users";
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(remove);
+            System.out.println("The table remove successful!");
+        } catch (SQLException e) {
+            System.out.println("<No>");
+        }
     }
 
     public List<User> getAllUsers() {
+        String getAllUsers = "SELECT * FROM users";
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(getAllUsers);
+            List<User> users = new ArrayList<>();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+                return users;
+            }
+        } catch (SQLException e) {
+            System.out.println("!!!");
+        }
         return null;
     }
 
-    public void cleanUsersTable() {
 
+    public void cleanUsersTable() {
+        String clean = "DELETE FROM users";
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(clean);
+            System.out.println("The table clean successful!");
+        } catch (SQLException e) {
+            System.out.println("!!!");
+        }
     }
 }
